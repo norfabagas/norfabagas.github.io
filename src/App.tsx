77f4317d7e7a21a9a2b924895ce4143161571910
Norfa Bagas <norfa_b_nurhuda@ekahospital.com>
2024-10-17 17:11:32 +0700

    Added shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,12 +20,11 @@ function App() {
         </div>
         <h1 className="text-4xl font-bold">Vite + React + Tailwind</h1>
         <div className="card mt-4">
-          <button
-            className="px-4 py-2 bg-blue-500 text-white rounded"
+          <Button
             onClick={() => setCount((count) => count + 1)}
           >
             Count is {count}
-          </button>
+          </Button>
         </div>
         <p className="read-the-docs text-gray-600">
           Click on the Vite and React logos to learn more
