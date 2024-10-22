5912d637f470a3769c403bfcb431f5793f0f908a
Norfa Bagas <bgsnorfa@gmail.com>
2024-10-22 16:27:08 +0700

    Fix shadcn installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-4xl bg-yellow-200">Hello, World</h1>
+      <Button variant="destructive">Button</Button>
     </div>
   );
 }
