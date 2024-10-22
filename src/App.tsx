0fe72903ad1175c144e6c5fc96fd731e8c1b4dc6
Norfa Bagas <bgsnorfa@gmail.com>
2024-10-22 14:56:40 +0700

    Re-setup Vite, tailwindcss, react, typescript + swc

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { Button } from './components/ui/button';
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <div className="mb-4">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1 className="text-4xl font-bold">Vite + React + Tailwind</h1>
-        <div className="card mt-4">
-          <Button
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Count is {count}
-          </Button>
-        </div>
-        <p className="read-the-docs text-gray-600">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-    </>
-  )
+    <div>
+      <h1 className="text-4xl bg-yellow-200">Hello, World</h1>
+    </div>
+  );
 }
 
 export default App;
